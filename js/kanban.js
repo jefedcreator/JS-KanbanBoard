fe081b438ca091502a794ebecaaf09e91009c371
jefedcreator <smallztin@gmail.com>
2021-09-30 05:00:26 +0100

    second commit

--- js/kanban.js
+++ js/kanban.js
@@ -19,9 +19,44 @@ const create_item = () => {
   let item = document.createElement("div")
   item.classList.add("item")
   item.setAttribute("id",`item-${order}`)
+  item.draggable = true;
+  item.addEventListener("dragstart",
+    (event)=>{
+        event.target.setData("text",event.target.id)
+    }
+  )
+  item.addEventListener("dragend",
+  (event)=>{
+    event.target.clearData()
+  })
+  let input = document.createElement("input")
+  item.appendChild(input)
+
+  let save_btn = document.createElement("button")
+  save_btn.innerHTML="Save";
+  save_btn.addEventListener("click",
+  ()=>{
+    error.innerHTML = "";
+  })
+  if (input.value === "") {
+    order += 1
+    item.innerHTML = input.value
+    adding = false
+  } else {
+    error.innerHTML = message;
+  }
+
+  item.appendChild(save_btn)
+  return item;
 };
 
-
-
 document.querySelectorAll('.drop').forEach(element => {
+  element.addEventListener("drop",(event)=>{
+    event.preventDefault()
+    const id = event.DataTransfer.getData("text")
+    event.target.appendChild(document.getElementById("id"))
+  })
+  element.addEventListener("dragover",(event)=>{
+    event.preventDefault()
+  })
 });
